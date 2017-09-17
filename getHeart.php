4e0011ec5fc7410f1deaa2104c729309ee53ca26
Luca Olivieri <gliolivieri@live.it>
2017-09-17 17:34:26 +0200

    Filling data gaps

--- getHeart.php
+++ getHeart.php
@@ -26,7 +26,7 @@ if ($sql->connect_error) {
     die($sql->connect_error);
 }
 
-if($data != false) { // Checking if there was a valid response
+if ($data != false) { // Checking if there was a valid response
     $values = json_decode($data, true);
     //var_dump($values);
 
@@ -34,20 +34,46 @@ if($data != false) { // Checking if there was a valid response
     $query = "TRUNCATE TABLE heartrate";
     $sql->query($query);
 
-    for($i = 0; $i < count($values['activities-heart-intraday']['dataset']); $i++) {
+    $data = [];
+    for ($i = 0; $i < count($values['activities-heart-intraday']['dataset']); $i++) {
         $time = $values['activities-heart-intraday']['dataset'][$i]['time'];
         $heart = $values['activities-heart-intraday']['dataset'][$i]['value'];
-        $query = "INSERT INTO heartrate VALUES('$time', $heart)";
-        $sql->query($query);
-        //echo $query . '<br>';
+
+        $data[$time] = $heart;
     }
 
-    echo date('H:i:s d-m-Y') . "\tDone updating database\n";
+    // Stuffing holes of no data, we want 1440 values for each day
+    for ($h = 0; $h < 24; $h++) {  // Hours
+        $hours = "$h:";
+        if(strlen($hours) == 2) { // Zero padding
+            $hours = "0" . $hours;
+        }
 
+        for($m = 0; $m < 60; $m++){  //Minutes
+            $minutes = "$m:00";
+            if(strlen($minutes) == 4) { // Zero padding
+                $minutes = "0" . $minutes;
+            }
+            $time = $hours . $minutes;
+            //echo $time . "<br>";
+
+            if (isset($data[$time])) { // If we have data
+                $query = "INSERT INTO heartrate VALUES('$time', $data[$time])";
+            } else { // If we don't have data
+                $query = "INSERT INTO heartrate VALUES('$time', 0)";
+            }
+
+            $sql->query($query);
+        }
+    }
+
+
+
+    echo date('H:i:s d-m-Y') . "\tDone updating database\n";
 } else {
     // Delete all data since the retreiving was unsuccesful
     $query = "TRUNCATE TABLE heartrate";
-    $sql->query($query);  
+    $sql->query($query);
 
     echo date('H:i:s d-m-Y') . "\tFailed updating database\n";
 }
